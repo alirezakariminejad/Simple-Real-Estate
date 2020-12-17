926f0b6c02aa71eaa5b6c8c6ca3f0d1184c13432
Alireza Kariminejad <alireza.kariminejad@yahoo.com>
2020-12-17 22:47:05 +0330

    add base class

---
+++ base.py
@@ -0,0 +1,21 @@
+from abc import ABC
+
+
+class BaseClass(ABC):
+    _id = 0
+    object_list = list()
+
+    def __init__(self, *args, **kwargs):
+        self._id = self.generate_id()
+        self.store(self)
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def generate_id(cls):
+        cls._id += 1
+        return cls._id
+
+    @classmethod
+    def store(cls, obj):
+        cls.object_list.append(obj)
+
